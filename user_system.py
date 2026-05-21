6ce3d21dd3c1e146e97d99333f5517ca58adf8fa
Chandak Apeksha Rahul <156424139+Apekshachandak@users.noreply.github.com>
2026-05-22 04:10:55 +0530

    Create user_system.py

---
+++ user_system.py
@@ -0,0 +1,25 @@
+# Function C (The bottom layer)
+def save_to_database(data):
+    if not data:
+        return False
+    print("Saved!")
+    return True
+
+# Function B (The middle layer)
+def register_user(username, age):
+    if age < 18:
+        return "Too young"
+    
+    # Notice it calls the bottom layer!
+    success = save_to_database(username)
+    return success
+
+# Function A (The top layer)
+def api_endpoint(request_data):
+    user = request_data.get("user")
+    age = request_data.get("age")
+    
+    # Notice it calls the middle layer!
+    if user and age:
+        return register_user(user, age)
+    return "Error"
