c72eef38e2b5cdf26bee9a08c46c26c272f3d7d8
Chandak Apeksha Rahul <156424139+Apekshachandak@users.noreply.github.com>
2026-05-23 03:07:49 +0530

    Add testing comments to vulnerable_app.py functions
    Added testing comments to functions for clarity.

--- vulnerable_app.py
+++ vulnerable_app.py
@@ -1,19 +1,23 @@
 # 1. SQL Injection 
 def get_user(username):
+    #testing
     query = "SELECT * FROM users WHERE username = '" + username + "'"
     cursor.execute(query)
     return cursor.fetchone()
 
 # 2. Command Injection 
 def check_host(hostname):
+    #testing
     import os
     os.system("ping -c 1 " + hostname)
 
 # 3. Weak Crypto 
 def store_password(password):
+    #testing
     import hashlib
     return hashlib.md5(password.encode()).hexdigest()
 
 # 4. Safe code 
 def add_numbers(a, b):
+    #testing
     return a + b
